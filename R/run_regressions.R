f4b2f53335177ba4e980fd64f82b9157a2db9810
Stijn Masschelein <stijnmasschelein@gmail.com>
2016-11-15 16:04:15 +0800

    added conditional correlation

--- R/run_regressions.R
+++ R/run_regressions.R
@@ -59,8 +59,17 @@ interaction_reg <- function(dat, method = "traditional"){
 #' @return A list with the sur object and the name of the method
 #' @export
 
+#' The SUR approach to detecting the complementarity
+#'
+#' @family tests
+#' @param dat A simulated dataset with 5 columns
+#' @seealso match_reg run_regression format_reg
+#'
+#' @return A list with the sur object and the name of the method
+
 sur_reg <- function(dat){
   stopifnot(ncol(dat) == nr_columns + 1)
+  # should be rewritten. See packages by Hadley Wickham
   if (!require("systemfit")){
     stop("The `systemfit` is not installed. Install `systemfit` to use the sur
          option")
@@ -68,6 +77,21 @@ sur_reg <- function(dat){
   reg <- systemfit::systemfit(list(eq1 = x1 ~ z, eq2 = x2 ~ z),
                               data = dat, method = "SUR", maxit = 100)
   return(list(reg = reg, method = "sur"))
+}
+
+#' The conditional correlation approach to detecting the complementarity
+#'
+#' @family tests
+#' @param dat A simulated dataset with 5 columns
+#' @seealso match_reg run_regression format_reg
+#'
+#' @return A list with the sur object and the name of the method
+#' @export
+
+cond_reg <- function(dat){
+  reg1 <- lm(x1 ~ z, data = dat)
+  reg2 <- lm(x2 ~ z, data = dat)
+  return(list(reg1 = reg1, reg2 = reg2, method = "conditional"))
 }
 
 #' A general function to run all possible regression based approaches.
@@ -126,6 +150,18 @@ format_reg <- function(list_reg){
                           coefficient = cor_resid,
                           r2 = summ_reg$ols.r.squared,
                           pvalue =  cortest$p.value,
+                          stat = cortest$statistic,
+                          stringsAsFactors = FALSE)
+  } else if(list_reg$method == "conditional"){
+    reg1 <- list_reg$reg1
+    reg2 <- list_reg$reg2
+    summ1 <- summary(reg1)
+    summ2 <- summary(reg2)
+    cor_test <- cor.test(resid(reg1), resid(reg2))
+    results <- data.frame(method = "conditional",
+                          coefficient = cor_test$statistic,
+                          r2 = mean(summ1$r.squared, summ2$r.squared),
+                          pvalue = cortest$p.value,
                           stat = cortest$statistic,
                           stringsAsFactors = FALSE)
   }
